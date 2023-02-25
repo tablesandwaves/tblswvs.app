bc87f18ef886162100f3474aaa8e6eb26e925bdf
Steve Meyer <stephen.meyer@wisc.edu>
2023-02-25 16:13:27 -0600

    Adding extra info to sketches

--- sketches/live_add_clip_notes.js
+++ sketches/live_add_clip_notes.js
@@ -1,10 +1,25 @@
 const OscEmitter  = require("osc-emitter");
+const OscReceiver = require("osc-receiver");
 const { Melody }  = require("tblswvs");
 
-const emitter  = new OscEmitter();
+
+const processClipNotes = (...response) => {
+  console.log("clip path", response[0], response[1]);
+  for (let i = 2; i < response.length; i += 5) {
+    console.log(response.slice(i, i + 5).join(" "));
+  }
+}
+
 
 // Establish communication TO Live
+const emitter  = new OscEmitter();
 emitter.add("localhost", 11000);
+
+// Establish communication FROM Live & and create a handler for receiving notes
+const receiver = new OscReceiver();
+receiver.bind(11001, "localhost");
+receiver.on("/live/clip/get/notes", processClipNotes)
+
 
 // Get half step offsets for the Infinity Series sequence
 const melodyOffsets = Melody.infinitySeries([0, 3]).steps;
@@ -15,13 +30,15 @@ let melodyData = melodyOffsets.flatMap((noteOffset, i) => {
     {type: "integer", value: 60 + noteOffset},
     {type: "double",  value: i * 0.25},
     {type: "double",  value: 0.25},
-    {type: "float",  value: 64},
-    {type: "boolean",  value: false},
+    {type: "float",   value: 64},
+    {type: "boolean", value: false},
   ]
 });
+
 
 // Track 1, Clip 1 are index 0, 0
 let firstTrackFirstClip = [{type: 'integer', value: 0}, {type: 'integer', value: 0}];
 
 // Tell Live to add the MIDI notes to the clip
 emitter.emit("/live/clip/add/notes", ...firstTrackFirstClip, ...melodyData);
+emitter.emit("/live/clip/get/notes", ...firstTrackFirstClip);
--- sketches/live_get_track_names.js
+++ sketches/live_get_track_names.js
@@ -2,6 +2,13 @@ const OscEmitter  = require("osc-emitter");
 const OscReceiver = require("osc-receiver");
 
 
+// Track name handler
+const processTrackNames = (...trackNames) => {
+  trackNames.forEach((trackName, i) => console.log(`${i + 1} ${trackName}`));
+  process.exit();
+}
+
+
 const emitter  = new OscEmitter();
 const receiver = new OscReceiver();
 
@@ -10,6 +17,11 @@ emitter.add("localhost", 11000);
 
 // Establish connection FROM Live and setup a call back to log the responses
 receiver.bind(11001, "localhost");
+
+// Create a specific handler for the track names
+receiver.on("/live/song/get/track_names", processTrackNames)
+
+// Create a generic handler for all messages
 receiver.on("message", (...response) => console.log(response[0], response.slice(1).join(", ")));
 
 // Request the track names from Live
