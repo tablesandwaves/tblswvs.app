b7a30a63bc24aed163d151802984386e9c4c41e6
Steve Meyer <stephen.meyer@wisc.edu>
2023-03-11 11:39:36 -0600

    Updating sketches for tblswvs.osc

--- sketches/live_add_clip_notes.js
+++ sketches/live_add_clip_notes.js
@@ -1,44 +1,89 @@
+/**
+ * Before running this script, open Live, create a MIDI track on track 1. Then add
+ * the tblswvs.osc.amxd device to the clip.
+ *
+ * Usage:
+ *
+ *  $ node sketches/live_add_clip_notes.js
+    Live responsed with Created in under 50ms
+    Live responsed with Success in under 50ms
+    Finishing Up.
+ */
+
+
 const OscEmitter  = require("osc-emitter");
 const OscReceiver = require("osc-receiver");
-const { Melody }  = require("tblswvs");
 
 
-const processClipNotes = (...response) => {
-  console.log("clip path", response[0], response[1]);
-  for (let i = 2; i < response.length; i += 5) {
-    console.log(response.slice(i, i + 5).join(" "));
-  }
+let liveResponse = undefined;
+
+const liveResponseHandler = (...response) => {
+  liveResponse = response[1];
 }
+
+const liveRequest = (liveApiCall, expectation, delay, repetitions) => {
+  liveApiCall();
+
+  return new Promise((resolve, reject) => {
+    let x = 1;
+    const intervalId = setInterval(() => {
+      if (liveResponse == expectation) {
+        clearInterval(intervalId);
+        liveResponse = undefined;
+        resolve([expectation, x * delay]);
+      }
+      if (++x === repetitions + 1) {
+        clearInterval(intervalId);
+        reject(x * delay);
+      }
+    }, delay);
+  });
+}
+
+
+const addTrackNotes = () => {
+  const melodyOffsets = [0, 3, 7];
+  const trackIndex    = 0;
+  const clipIndex     = 0;
+  const midiTonic     = 60;
+
+  let melodyData = melodyOffsets.flatMap((noteOffset, i) => [noteOffset + midiTonic, i * 0.25, 0.25, 64, 0.5]);
+  let address = `/tracks/${trackIndex}/clips/${clipIndex}/notes`;
+  emitter.emit(address, ...melodyData);
+}
+
+
+const createTrackClip = () => {
+  const trackIndex = 0;
+  const clipIndex  = 0;
+  let address = `/tracks/${trackIndex}/clips/${clipIndex}/create`;
+  emitter.emit(address, 4);
+}
+
+
+const fulfilledMsg = ([result, maxTime]) => console.log(`Live responsed with ${result} in under ${maxTime}ms`);
+const rejectedMsg  = (maxTime) => console.log(`Waited more than ${maxTime}ms for confirmed response`);
 
 
 // Establish communication TO Live
 const emitter  = new OscEmitter();
-emitter.add("localhost", 11000);
+emitter.add("localhost", 33333);
 
 // Establish communication FROM Live & and create a handler for receiving notes
 const receiver = new OscReceiver();
-receiver.bind(11001, "localhost");
-receiver.on("/live/clip/get/notes", processClipNotes)
+receiver.bind(33334, "localhost");
+receiver.on("message", liveResponseHandler);
 
+const responseWaitTime = 50;
+const maxNumberOfWaits = 3;
 
-// Get half step offsets for the Infinity Series sequence
-const melodyOffsets = Melody.infinitySeries([0, 3]).steps;
-
-// Translate offsets to Live note data format
-let melodyData = melodyOffsets.flatMap((noteOffset, i) => {
-  return [
-    {type: "integer", value: 60 + noteOffset},
-    {type: "double",  value: i * 0.25},
-    {type: "double",  value: 0.25},
-    {type: "float",   value: 64},
-    {type: "boolean", value: false},
-  ]
+liveRequest(createTrackClip, "Created", responseWaitTime, maxNumberOfWaits)
+.then(result   => fulfilledMsg(result))
+.then(()       => liveRequest(addTrackNotes, "Success", responseWaitTime, maxNumberOfWaits))
+.then(result   => fulfilledMsg(result))
+.catch(maxTime => rejectedMsg(maxTime))
+.finally(()    => {
+  console.log("Finishing Up.");
+  emitter._socket.close();
+  receiver._socket.close();
 });
-
-
-// Track 1, Clip 1 are index 0, 0
-let firstTrackFirstClip = [{type: 'integer', value: 0}, {type: 'integer', value: 0}];
-
-// Tell Live to add the MIDI notes to the clip
-emitter.emit("/live/clip/add/notes", ...firstTrackFirstClip, ...melodyData);
-emitter.emit("/live/clip/get/notes", ...firstTrackFirstClip);
