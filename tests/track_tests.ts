071ab4c3ca6089cbd785e3db4aa49ee0429eeab0
Steve Meyer <stephen.meyer@wisc.edu>
2023-04-23 09:25:37 -0500

    Moving older tests for sequencer into tests for track

--- tests/track_tests.ts
+++ tests/track_tests.ts
@@ -8,11 +8,43 @@ const testing   = true;
 const sequencer = new Sequencer(testing);
 const daw       = new AbletonLive(sequencer);
 const track     = new AbletonTrack("Kick", daw, 0);
+sequencer.superMeasure = 2;
 
 
 describe("AbletonTrack", () => {
+  describe("generating Ableton notes for a track", () => {
+    describe("with a beat length of 12 16th notes", () => {
+      track.beatLength  = 12;
+      track.rhythm      = new Array(12).fill({...{state: 0, probability: 1, fillRepeats: 0}});
+      track.rhythm[0]   = {state: 1, probability: 1, fillRepeats: 0};
+      track.outputNotes = [
+        [{ octave: 3, note: 'C', midi: 60, scaleDegree: 1 }],
+        [{ octave: 3, note: 'Eb', midi: 63, scaleDegree: 3 }],
+        [{ octave: 3, note: 'G', midi: 67, scaleDegree: 5 }]
+      ];
+      let abletonNotes = track.abletonNotes();
+
+
+      it("should the have correct number of notes", () => {
+        expect(abletonNotes.length).to.eq(3);
+      });
+
+      it("should have the right notes in order", () => {
+        expect(abletonNotes[0].midiNote).to.eq(60);
+        expect(abletonNotes[1].midiNote).to.eq(63);
+        expect(abletonNotes[2].midiNote).to.eq(67);
+      });
+
+      it("should place the notes at the correct positions", () => {
+        expect(abletonNotes[0].clipPosition).to.eq(0);
+        expect(abletonNotes[1].clipPosition).to.eq(3);
+        expect(abletonNotes[2].clipPosition).to.eq(6);
+      });
+    });
+  });
+
   describe("when generating rhythmic fills", () => {
-    sequencer.superMeasure = 2;
+    track.beatLength       = 16;
     track.outputNotes      = [ [{ octave: 3, note: 'C', midi: 60, scaleDegree: 1 }] ];
     track.rhythm           = new Array(16).fill({...{state: 0, probability: 1, fillRepeats: 0}});
     track.rhythm[0]        = {state: 1, probability: 1, fillRepeats: 3};
@@ -42,6 +74,7 @@ describe("AbletonTrack", () => {
 
 
   describe("truncating note durations for overlapping notes", () => {
+    track.beatLength  = 16;
     track.noteLength  = "8n";
     track.rhythm      = new Array(16).fill({...{state: 0, probability: 1, fillRepeats: 0}});
     track.rhythm[0]   = {state: 1, probability: 1, fillRepeats: 0};
