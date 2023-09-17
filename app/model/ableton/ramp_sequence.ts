64b27b92815f3c4ddced143045c5969394e27157
Steve Meyer <stephen.meyer@wisc.edu>
2023-09-16 21:22:42 -0500

    RampSequence object as container for tblswvs.rampseq M4L device

---
+++ app/model/ableton/ramp_sequence.ts
@@ -0,0 +1,110 @@
+export type RampRange = {
+  start: number,
+  end: number
+}
+
+
+export type RampSegment = {
+  startIndex: number,
+  length: number,
+  subdivisionLength: number,
+  range: RampRange
+}
+
+
+export class RampSequence {
+  segments: RampSegment[] = new Array();
+
+
+  addSegment(index: number) {
+    if (index < 0) index = 0;
+    if (index > 15) index = 15;
+
+    this.segments.push({
+      startIndex: index,
+      length: -1,
+      subdivisionLength: -1,
+      range: {start: 0, end: 1}
+    });
+
+    this.#rebalance();
+  }
+
+
+  removeSegment(index: number) {
+    // Capture the state just before the segment is removed so you know which segments should have their subdivisions
+    // set to full segment length when rebalancing.
+    const fullLengthSubdivisionSegments = this.segments.map(segment => segment.length == segment.subdivisionLength);
+    const removalIndex = this.segments.findIndex(segment => segment.startIndex == index);
+
+    this.segments.splice(removalIndex, 1);
+    fullLengthSubdivisionSegments.splice(removalIndex, 1);
+
+    this.#rebalance(fullLengthSubdivisionSegments);
+  }
+
+
+  #rebalance(fullLengthSubdivisionSegments?: boolean[]) {
+    this.segments.sort((a, b) => a.startIndex - b.startIndex);
+    this.segments.forEach((segment, i, arr) => {
+      segment.length = arr[i + 1] == undefined ? 16 - segment.startIndex : arr[i + 1].startIndex - segment.startIndex;
+      if (segment.subdivisionLength == -1 ||
+        (fullLengthSubdivisionSegments && fullLengthSubdivisionSegments[i]) ||
+        segment.subdivisionLength > segment.length) {
+        segment.subdivisionLength = segment.length;
+      }
+    });
+  }
+
+
+  updateSubdivisionLength(segmentStartIndex: number, subdivisionLength: number) {
+    const segment = this.segments.find(segment => segment.startIndex == segmentStartIndex);
+    if (subdivisionLength >= 1 && subdivisionLength <= segment.length) {
+      segment.subdivisionLength = subdivisionLength;
+    }
+  }
+
+
+  updateRange(segmentStartIndex: number, start: number, end: number) {
+    if (start < 0) start = 0;
+    if (start > 1) start = 1;
+    if (end < 0)   end   = 0;
+    if (end > 1)   end   = 1;
+
+    this.segments.find(segment => segment.startIndex == segmentStartIndex).range.start = start;
+    this.segments.find(segment => segment.startIndex == segmentStartIndex).range.end = end;
+  }
+
+
+  gridSegmentRow(): (0|1)[] {
+    if (this.segments.length == 0) return new Array(16).fill(0);
+
+    return this.segments.reduce((row, segment) => {
+      const subsegment = new Array(segment.length).fill(0);
+      subsegment[0] = 1;
+      return row.concat(subsegment);
+    }, new Array());
+  }
+
+
+  gridSubdivisionRow(): (0|1)[] {
+    if (this.segments.length == 0) return new Array(16).fill(0);
+
+    return this.segments.reduce((row, segment) => {
+      return row.concat(new Array(segment.subdivisionLength).fill(1))
+                .concat(new Array(segment.length - segment.subdivisionLength).fill(0));
+    }, new Array());
+  }
+
+
+  gridRangeRow(segmentStartIndex: number): (0|1)[] {
+    if (this.segments.length == 0) return new Array(16).fill(0);
+
+    const segment = this.segments.find(segment => segment.startIndex == segmentStartIndex);
+
+    return [...new Array(16)].map((e, i) => {
+      if (i / 16 >= segment.range.start && i / 16 <= segment.range.end) return 1;
+      return 0;
+    });
+  }
+}
---
+++ tests/ramp_sequence_tests.ts
@@ -0,0 +1,316 @@
+import { expect } from "chai";
+import { RampSequence } from "../app/model/ableton/ramp_sequence";
+
+
+describe("RampSequence", () => {
+  describe("an empty sequence", () => {
+    const rampSequence = new RampSequence();
+
+    it("has an empty segment array", () => {
+      expect(rampSequence.segments.length).to.eq(0);
+    });
+
+    it("has an all-zeroes grid segment row", () => {
+      expect(rampSequence.gridSegmentRow()).to.have.ordered.members(
+        [0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      );
+    });
+
+    it("has an all-zeroes grid subdivisions row", () => {
+      expect(rampSequence.gridSubdivisionRow()).to.have.ordered.members(
+        [0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      );
+    });
+
+    it("has an all-zeroes grid range row", () => {
+      expect(rampSequence.gridSubdivisionRow()).to.have.ordered.members(
+        [0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      );
+    });
+  })
+
+
+  describe("adding the first outer steps at position 0", () => {
+    const rampSequence = new RampSequence();
+    rampSequence.addSegment(0);
+
+    it("should have one segment", () => {
+      expect(rampSequence.segments.length).to.eq(1);
+    });
+
+    it("should store the outer gate", () => {
+      expect(rampSequence.segments[0].startIndex).to.eq(0);
+    });
+
+    it("should set the default length", () => {
+      expect(rampSequence.segments[0].length).to.eq(16);
+    });
+
+    it("should set the default value for the subdivision length", () => {
+      expect(rampSequence.segments[0].subdivisionLength).to.eq(16);
+    });
+
+    it("should set the default value for the range", () => {
+      expect(rampSequence.segments[0].range.start).to.eq(0);
+      expect(rampSequence.segments[0].range.end).to.eq(1);
+    });
+
+    it("can generate a grid segment row", () => {
+      expect(rampSequence.gridSegmentRow()).to.have.ordered.members(
+        [1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      )
+    });
+
+    it("can generate a grid subdivision row", () => {
+      expect(rampSequence.gridSubdivisionRow()).to.have.ordered.members(
+        [1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1]
+      )
+    });
+
+    it("can generate a grid range row", () => {
+      expect(rampSequence.gridRangeRow(0)).to.have.ordered.members(
+        [1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1]
+      )
+    });
+
+    it("can have its subdivision length updated, which updates the grid subdivision row", () => {
+      const rampSequence = new RampSequence();
+      rampSequence.addSegment(0);
+      rampSequence.updateSubdivisionLength(0, 5);
+      expect(rampSequence.segments[0].subdivisionLength).to.eq(5);
+      expect(rampSequence.gridSubdivisionRow()).to.have.ordered.members(
+        [1, 1, 1, 1,  1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      )
+    });
+
+    it("can have its range updated, which updates the grid range row", () => {
+      rampSequence.updateRange(0, 0.25, 0.75);
+      expect(rampSequence.segments[0].range.start).to.eq(0.25);
+      expect(rampSequence.segments[0].range.end).to.eq(0.75);
+      expect(rampSequence.gridRangeRow(0)).to.have.ordered.members(
+        [0, 0, 0, 0,  1, 1, 1, 1,  1, 1, 1, 1,  1, 0, 0, 0]
+      )
+    });
+  });
+
+
+  describe("adding multiple steps", () => {
+    const rampSequence = new RampSequence();
+    rampSequence.addSegment(0);
+    rampSequence.addSegment(6);
+
+    it("should have two segments", () => {
+      expect(rampSequence.segments.length).to.eq(2);
+    });
+
+    it("should store the outer gates", () => {
+      expect(rampSequence.segments[0].startIndex).to.eq(0);
+      expect(rampSequence.segments[1].startIndex).to.eq(6);
+    });
+
+    it("should set the default length and adjust the existing length", () => {
+      expect(rampSequence.segments[0].length).to.eq(6);
+      expect(rampSequence.segments[1].length).to.eq(10);
+    });
+
+    it("should set the default value for the subdivision length", () => {
+      expect(rampSequence.segments[0].subdivisionLength).to.eq(6);
+      expect(rampSequence.segments[1].subdivisionLength).to.eq(10);
+    });
+
+    it("can generate a grid segment row", () => {
+      expect(rampSequence.gridSegmentRow()).to.have.ordered.members(
+        [1, 0, 0, 0,  0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      )
+    });
+
+    it("can generate a grid subdivision row", () => {
+      expect(rampSequence.gridSubdivisionRow()).to.have.ordered.members(
+        [1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1,  1, 1, 1, 1]
+      )
+    });
+  });
+
+
+  describe("adding a segment that alters a previous segment's subdivision length", () => {
+    const rampSequence = new RampSequence();
+    rampSequence.addSegment(0);
+    rampSequence.updateSubdivisionLength(0, 8);
+    rampSequence.addSegment(6);
+    rampSequence.updateSubdivisionLength(6, 3);
+
+    it("should have two segments", () => {
+      expect(rampSequence.segments.length).to.eq(2);
+    });
+
+    it("should store the outer gates", () => {
+      expect(rampSequence.segments[0].startIndex).to.eq(0);
+      expect(rampSequence.segments[1].startIndex).to.eq(6);
+    });
+
+    it("should set the default length and adjust the existing length", () => {
+      expect(rampSequence.segments[0].length).to.eq(6);
+      expect(rampSequence.segments[1].length).to.eq(10);
+    });
+
+    it("does not allow a subdivision length to be greater than the segment length", () => {
+      expect(rampSequence.segments[0].subdivisionLength).to.eq(6);
+      expect(rampSequence.segments[1].subdivisionLength).to.eq(3);
+    });
+
+    it("can generate a grid segment row", () => {
+      expect(rampSequence.gridSegmentRow()).to.have.ordered.members(
+        [1, 0, 0, 0,  0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      )
+    });
+
+    it("can generate a grid subdivision row", () => {
+      expect(rampSequence.gridSubdivisionRow()).to.have.ordered.members(
+        [1, 1, 1, 1,  1, 1, 1, 1,  1, 0, 0, 0,  0, 0, 0, 0]
+      )
+    });
+  });
+
+
+  describe("segment validation", () => {
+    it("will not start a segment at an index less than 0", () => {
+      const rampSequence = new RampSequence();
+      rampSequence.addSegment(-1);
+      expect(rampSequence.segments[0].startIndex).to.eq(0);
+    });
+
+    it("will not start a segment at an index greater than 15", () => {
+      const rampSequence = new RampSequence();
+      rampSequence.addSegment(16);
+      expect(rampSequence.segments[0].startIndex).to.eq(15);
+      expect(rampSequence.segments[0].length).to.eq(1);
+    });
+  });
+
+
+  describe("subdivision validation", () => {
+    const rampSequence = new RampSequence();
+    rampSequence.addSegment(0);
+
+    it("will not set a subdivision length greater than the segment length", () => {
+      rampSequence.updateSubdivisionLength(0, 17);
+      expect(rampSequence.segments[0].subdivisionLength).to.eq(16);
+    });
+
+    it("will not set a subdivision length less than 1", () => {
+      rampSequence.updateSubdivisionLength(0, 0);
+      expect(rampSequence.segments[0].subdivisionLength).to.eq(16);
+    });
+
+    it("will not set a subdivision that would run past the 16 step limit", () => {
+      rampSequence.addSegment(14);
+      rampSequence.updateSubdivisionLength(14, 3);
+      expect(rampSequence.segments[1].subdivisionLength).to.eq(2);
+    });
+  });
+
+
+  describe("range validation", () => {
+    const rampSequence = new RampSequence();
+    rampSequence.addSegment(0);
+
+    it("will not set a range below 0", () => {
+      rampSequence.updateRange(0, -0.25, 0.75);
+      expect(rampSequence.segments[0].range.start).to.eq(0);
+      expect(rampSequence.segments[0].range.end).to.eq(0.75);
+    });
+
+    it("will not set a range greater than 1", () => {
+      rampSequence.updateRange(0, 0.25, 1.25);
+      expect(rampSequence.segments[0].range.start).to.eq(0.25);
+      expect(rampSequence.segments[0].range.end).to.eq(1);
+    });
+
+    it("will not set a range below 0 when high to low", () => {
+      rampSequence.updateRange(0, 0.75, -0.25);
+      expect(rampSequence.segments[0].range.start).to.eq(0.75);
+      expect(rampSequence.segments[0].range.end).to.eq(0);
+    });
+
+    it("will not set a range greater than 1 when high to low", () => {
+      rampSequence.updateRange(0, 1.25, 0.25);
+      expect(rampSequence.segments[0].range.start).to.eq(1);
+      expect(rampSequence.segments[0].range.end).to.eq(0.25);
+    });
+
+    it("will allow a static range", () => {
+      rampSequence.updateRange(0, 0.5, 0.5);
+      expect(rampSequence.segments[0].range.start).to.eq(0.5);
+      expect(rampSequence.segments[0].range.end).to.eq(0.5);
+    })
+  });
+
+
+  describe("removing a segment", () => {
+    it("can remove a single segment", () => {
+      const rampSequence = new RampSequence();
+      rampSequence.addSegment(0);
+      expect(rampSequence.segments.length).to.eq(1);
+      expect(rampSequence.gridSegmentRow()).to.have.ordered.members(
+        [1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      );
+
+      rampSequence.removeSegment(0);
+      expect(rampSequence.segments.length).to.eq(0);
+      expect(rampSequence.gridSegmentRow()).to.have.ordered.members(
+        [0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+      );
+    });
+
+
+    describe("rebalancing the last segment after removal from a multi-segment sequence", () => {
+      it("will extend a subdivision when it is equal to its length", () => {
+        const rampSequence = new RampSequence();
+        rampSequence.addSegment(0);
+        expect(rampSequence.segments.length).to.eq(1);
+        expect(rampSequence.segments[0].subdivisionLength).to.eq(16);
+
+        rampSequence.addSegment(4);
+        expect(rampSequence.segments.length).to.eq(2);
+        expect(rampSequence.segments[0].subdivisionLength).to.eq(4);
+        expect(rampSequence.segments[1].subdivisionLength).to.eq(12);
+
+        rampSequence.addSegment(8);
+        expect(rampSequence.segments.length).to.eq(3);
+        expect(rampSequence.segments[0].subdivisionLength).to.eq(4);
+        expect(rampSequence.segments[1].subdivisionLength).to.eq(4);
+        expect(rampSequence.segments[2].subdivisionLength).to.eq(8);
+
+        rampSequence.removeSegment(8);
+        expect(rampSequence.segments.length).to.eq(2);
+        expect(rampSequence.segments[0].subdivisionLength).to.eq(4);
+        expect(rampSequence.segments[1].subdivisionLength).to.eq(12);
+      });
+    });
+
+    describe("rebalancing a non-last after removal from a multi-segment sequence", () => {
+      it("will extend a subdivision when it is equal to its length", () => {
+        const rampSequence = new RampSequence();
+        rampSequence.addSegment(0);
+        expect(rampSequence.segments.length).to.eq(1);
+        expect(rampSequence.segments[0].subdivisionLength).to.eq(16);
+
+        rampSequence.addSegment(4);
+        expect(rampSequence.segments.length).to.eq(2);
+        expect(rampSequence.segments[0].subdivisionLength).to.eq(4);
+        expect(rampSequence.segments[1].subdivisionLength).to.eq(12);
+
+        rampSequence.addSegment(8);
+        expect(rampSequence.segments.length).to.eq(3);
+        expect(rampSequence.segments[0].subdivisionLength).to.eq(4);
+        expect(rampSequence.segments[1].subdivisionLength).to.eq(4);
+        expect(rampSequence.segments[2].subdivisionLength).to.eq(8);
+
+        rampSequence.removeSegment(4);
+        expect(rampSequence.segments.length).to.eq(2);
+        expect(rampSequence.segments[0].subdivisionLength).to.eq(8);
+        expect(rampSequence.segments[1].subdivisionLength).to.eq(8);
+      });
+    });
+  });
+});
