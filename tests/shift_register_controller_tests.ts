c3b73660dd7e4b13e8f756569307bf0dc8676710
Steve Meyer <stephen.meyer@wisc.edu>
2024-03-18 19:57:04 -0500

    Basic shift register controller tests

---
+++ tests/shift_register_controller_tests.ts
@@ -0,0 +1,67 @@
+import { expect } from "chai";
+import { Sequencer } from "../app/model/sequencer";
+import { ShiftRegisterController } from "../app/controller/shift_register_controller";
+import { AlgorithmController } from "../app/controller/algorithm_controller";
+import { configDirectory, patternForRhythmSteps } from "./test_helpers";
+
+
+const testing   = true;
+
+
+describe("ShiftRegisterController", () => {
+  describe("After selecting the initial algorithm page", () => {
+    const sequencer = new Sequencer(configDirectory, testing);
+
+    // Select the algorithm page, then the shift register sub-page
+    sequencer.grid.keyPress({y: 7, x: 11, s: 1});
+    sequencer.grid.keyPress({y: 6, x: 1, s: 1});
+    const activePage = sequencer.grid.activePage as ShiftRegisterController;
+
+    it("the shift register controller can be selected", () => {
+      expect(activePage).to.be.instanceOf(ShiftRegisterController)
+    });
+
+    describe("the default shift register controller state", () => {
+      it("has the default shift register length row", () => expect(activePage.getShiftRegisterLengthRow()).to.have.ordered.members([
+        10, 10, 10, 10,  10, 10, 10, 10
+      ]));
+
+      it("has the default shift register chance row", () => expect(activePage.getShiftRegisterChanceRow()).to.have.ordered.members([
+        10, 10, 10, 10,  10, 10, 10, 10
+      ]));
+
+      it("has the default shift register range row", () => expect(activePage.getShiftRegisterRangeRow()).to.have.ordered.members([
+        0, 10, 10, 0,  0, 0, 0, 0
+      ]));
+    });
+  });
+
+
+  describe("Updating shift register parameters", () => {
+    const sequencer = new Sequencer(configDirectory, testing);
+    const track = sequencer.daw.getActiveTrack();
+
+    // Select the algorithm page, then the shift register sub-page
+    sequencer.grid.keyPress({y: 7, x: 11, s: 1});
+    sequencer.grid.keyPress({y: 6, x: 1, s: 1});
+    const activePage = sequencer.grid.activePage as ShiftRegisterController;
+
+    it("can update the track's shift register length", () => {
+      sequencer.grid.keyPress({y: 2, x: 2, s: 1});
+      expect(track.shiftRegister.length).to.eq(3);
+    });
+
+    it("can update the track's shift register chance", () => {
+      sequencer.grid.keyPress({y: 3, x: 2, s: 1});
+      expect(track.shiftRegister.chance).to.eq(0.375);
+    });
+
+    it("can update the track's shift register range", () => {
+      sequencer.grid.keyPress({y: 2, x: 8, s: 1});
+      sequencer.grid.keyPress({y: 2, x: 10, s: 1});
+      sequencer.grid.keyPress({y: 2, x: 8, s: 0});
+      sequencer.grid.keyPress({y: 2, x: 10, s: 0});
+      expect(track.shiftRegisterOctaveRange).to.have.ordered.members([1, 1, 1, 0]);
+    });
+  });
+});
