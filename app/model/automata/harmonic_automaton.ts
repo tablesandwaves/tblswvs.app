0e9f7b96ffe42c376ff2ddc7d5d6c2ab5eabc1c3
Steve Meyer <steve.e.meyer@gmail.com>
2024-12-08 16:07:40 -0600

    Attack and duration params are state machines

--- app/model/automata/harmonic_automaton.ts
+++ app/model/automata/harmonic_automaton.ts
@@ -1,7 +1,14 @@
+import { NamedRandomStateMachine } from "./named_random_state_machine";
+import { RandomStateMachine } from "./random_state_machine";
+
 export class HarmonicAutomaton {
   name: string;
+  attack: RandomStateMachine;
+  duration: NamedRandomStateMachine;
 
   constructor(automatonSpec: any) {
     this.name = automatonSpec.name;
+    this.attack = new RandomStateMachine(automatonSpec.parameters.attack.choices);
+    this.duration = new NamedRandomStateMachine(automatonSpec.parameters.duration.choices);
   }
 }
--- tests/harmonic_automaton_tests.ts
+++ tests/harmonic_automaton_tests.ts
@@ -4,6 +4,8 @@ import * as path from "path";
 import { expect } from "chai";
 import { before } from "mocha";
 import { HarmonicAutomaton } from "../app/model/automata/harmonic_automaton";
+import { RandomStateMachine } from "../app/model/automata/random_state_machine";
+import { NamedRandomStateMachine } from "../app/model/automata/named_random_state_machine";
 import { configDirectory, rhythmStepsForPattern } from "./test_helpers";
 
 
@@ -20,8 +22,30 @@ describe("HarmonicAutomaton", () => {
       );
     });
 
-    it("should have a name", () => {
+    it("has a name", () => {
       expect(automaton.name).to.eq("standard");
+    });
+
+
+    describe("the attack parameter", () => {
+      it("is a random state machine", () => {
+        expect(automaton.attack).to.be.an.instanceOf(RandomStateMachine);
+      });
+
+      it("has a list of choices", () => {
+        expect(automaton.attack.choices).to.have.ordered.members([0, 0.25, 0.5, 0.5, 0.75, 0.75, 1, 1]);
+      });
+    });
+
+
+    describe("the duration parameter", () => {
+      it("is a named random state machine", () => {
+        expect(automaton.duration).to.be.an.instanceOf(NamedRandomStateMachine);
+      });
+
+      it("has named choices", () => {
+        expect(automaton.duration.next("medium")).to.be.oneOf(["0.1.0", "0.2.0"])
+      });
     });
   });
 });
