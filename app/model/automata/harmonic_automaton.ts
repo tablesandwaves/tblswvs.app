f1de3aef0af44766e1af58c18f0797f1f6516c2f
Steve Meyer <steve.e.meyer@gmail.com>
2024-12-08 22:08:34 -0600

    Remaining automaton properties

--- app/model/automata/harmonic_automaton.ts
+++ app/model/automata/harmonic_automaton.ts
@@ -4,16 +4,35 @@ import { RandomStateMachine } from "./random_state_machine";
 import { RangeStateMachine } from "./range_state_machine";
 
 
+export type chordType = {
+  type: "triad"|"dyad",
+  interval?: number,
+  root?: number,
+  offset?: number
+}
+
+
 export class HarmonicAutomaton {
   name: string;
   attack: RandomStateMachine;
   duration: NamedRandomStateMachine;
   velocity: PatternStateMachine;
   filterFrequency: RangeStateMachine;
+  minMelodyIterations: number;
+  minChordIterations: number;
+  melodyDurationSize: RandomStateMachine;
+  chordDurationSize: RandomStateMachine;
+  melodyStartDegree: RandomStateMachine;
+  chordStartRoot: RandomStateMachine;
+  chordStartQuality: RandomStateMachine;
+  melodyNoteDistance: NamedRandomStateMachine;
+  chord: NamedRandomStateMachine;
 
 
   constructor(automatonSpec: any) {
     this.name = automatonSpec.name;
+    this.minMelodyIterations = automatonSpec.parameters.minMelodyIterations;
+    this.minChordIterations = automatonSpec.parameters.minChordIterations;
     this.attack = new RandomStateMachine(automatonSpec.parameters.attack.choices);
     this.duration = new NamedRandomStateMachine(automatonSpec.parameters.duration.choices);
     this.velocity = new PatternStateMachine(automatonSpec.parameters.velocity.choices);
@@ -21,5 +40,12 @@ export class HarmonicAutomaton {
       automatonSpec.parameters.filterFrequency.min,
       automatonSpec.parameters.filterFrequency.max
     );
+    this.melodyDurationSize = new RandomStateMachine(automatonSpec.parameters.melodyDurationSize.choices);
+    this.chordDurationSize = new RandomStateMachine(automatonSpec.parameters.chordDurationSize.choices);
+    this.melodyStartDegree = new RandomStateMachine(automatonSpec.parameters.melodyStartDegree.choices);
+    this.chordStartRoot = new RandomStateMachine(automatonSpec.parameters.chordStartRoot.choices);
+    this.chordStartQuality = new RandomStateMachine(automatonSpec.parameters.chordStartQuality.choices);
+    this.melodyNoteDistance = new NamedRandomStateMachine(automatonSpec.parameters.melodyNoteDistance.choices);
+    this.chord = new NamedRandomStateMachine(automatonSpec.parameters.chord.choices);
   }
 }
--- tests/harmonic_automaton_tests.ts
+++ tests/harmonic_automaton_tests.ts
@@ -3,7 +3,7 @@ import * as yaml from "js-yaml";
 import * as path from "path";
 import { expect } from "chai";
 import { before } from "mocha";
-import { HarmonicAutomaton } from "../app/model/automata/harmonic_automaton";
+import { HarmonicAutomaton, chordType } from "../app/model/automata/harmonic_automaton";
 import { RandomStateMachine } from "../app/model/automata/random_state_machine";
 import { NamedRandomStateMachine } from "../app/model/automata/named_random_state_machine";
 import { PatternStateMachine } from "../app/model/automata/pattern_state_machine";
@@ -21,9 +21,9 @@ describe("HarmonicAutomaton", () => {
       );
     });
 
-    it("has a name", () => {
-      expect(automaton.name).to.eq("standard");
-    });
+    it("has a name", () => expect(automaton.name).to.eq("standard"));
+    it("has a min melody iterations", () => expect(automaton.minMelodyIterations).to.eq(20));
+    it("has a min chord iterations", () => expect(automaton.minChordIterations).to.eq(4));
 
 
     describe("the attack parameter", () => {
@@ -68,7 +68,93 @@ describe("HarmonicAutomaton", () => {
       });
 
       it("generates random frequencies within range", () => {
-        expect(automaton.filterFrequency.next()).to.be.within(200, 16000)
+        expect(automaton.filterFrequency.next()).to.be.within(200, 16000);
+      });
+    });
+
+
+    describe("the melody duration size parameter", () => {
+      it("is a random state machine", () => {
+        expect(automaton.melodyDurationSize).to.be.an.instanceOf(RandomStateMachine);
+      });
+
+      it("generates random duration sizes", () => {
+        expect(automaton.melodyDurationSize.next()).to.be.oneOf(["small", "medium"]);
+      });
+    });
+
+
+    describe("the chord duration size parameter", () => {
+      it("is a random state machine", () => {
+        expect(automaton.chordDurationSize).to.be.an.instanceOf(RandomStateMachine);
+      });
+
+      it("generates random duration sizes", () => {
+        expect(automaton.chordDurationSize.next()).to.be.oneOf(["medium", "large"]);
+      });
+    });
+
+
+    describe("the melody start degree parameter", () => {
+      it("is a random state machine", () => {
+        expect(automaton.melodyStartDegree).to.be.an.instanceOf(RandomStateMachine);
+      });
+
+      it("generates random duration sizes", () => {
+        expect(automaton.melodyStartDegree.next()).to.be.oneOf([1, 2, 3, 4, 5, 6, 7]);
+      });
+    });
+
+
+    describe("the chord start root parameter", () => {
+      it("is a random state machine", () => {
+        expect(automaton.chordStartRoot).to.be.an.instanceOf(RandomStateMachine);
+      });
+
+      it("generates random duration sizes", () => {
+        expect(automaton.chordStartRoot.next()).to.be.oneOf([1, 3, 5]);
+      });
+    });
+
+
+    describe("the chord start quality parameter", () => {
+      it("is a random state machine", () => {
+        expect(automaton.chordStartQuality).to.be.an.instanceOf(RandomStateMachine);
+      });
+
+      it("generates a random chord type", () => {
+        expect(automaton.chordStartQuality.next()).to.haveOwnProperty("type").that.is.oneOf(["triad", "dyad"]);
+      });
+    });
+
+
+    describe("the melody note distance parameter", () => {
+      it("is a named random state machine", () => {
+        expect(automaton.melodyNoteDistance).to.be.an.instanceOf(NamedRandomStateMachine);
+      });
+
+      it("generates a random melody note distance", () => {
+        expect(automaton.melodyNoteDistance.next("small")).to.be.oneOf(["small", "medium", "large"]);
+      });
+    });
+
+
+    describe("the chord parameter", () => {
+      it("is a named random state machine", () => {
+        expect(automaton.chord).to.be.an.instanceOf(NamedRandomStateMachine);
+      });
+
+      it("generates a specific chord when one is configured", () => {
+        expect(automaton.chord.next("dim")).to.have.property("type", "triad");
+        expect(automaton.chord.next("dim")).to.have.property("degreeOffset", 2);
+      });
+
+      it("generates a specific chord when multiple are configured", () => {
+        expect(automaton.chord.next("m").interval).to.be.oneOf([4, 6]);
+      });
+
+      it("generates a random chord when no there is no other match", () => {
+        expect(automaton.chord.next("default")).to.haveOwnProperty("type").that.is.oneOf(["triad", "dyad"]);
       });
     });
   });
